4fca5aff4ea058edfcc33001c5df39a70ee46781
Cesar <cesaring@hotmail.com>
2021-08-16 20:32:58 -0500

    Added API Support

---
+++ app/src/main/java/quotes/app/QuoteAPI.java
@@ -0,0 +1,18 @@
+package quotes.app;
+
+import java.util.List;
+
+public class QuoteAPI{
+    public int id;
+    public boolean dialogue;
+    public boolean _private;
+    public List<Object> tags;
+    public String url;
+    public int favorites_count;
+    public int upvotes_count;
+    public int downvotes_count;
+    public String author;
+    public String author_permalink;
+    public String body;
+}
+
---
+++ app/src/main/java/quotes/app/QuoteOfTheDay.java
@@ -0,0 +1,9 @@
+package quotes.app;
+
+import java.util.Date;
+
+public class QuoteOfTheDay{
+    public Date qotd_date;
+    public QuoteAPI quote;
+}
+
